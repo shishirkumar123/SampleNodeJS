55741ce4d8f6f92f789802709ce1792ac864d427
Shishir <shishirkumar123@gmail.com>
2024-09-13 15:40:00 -0700

    c

---
+++ basic/app2a.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+// Read a file asynchronously
+fs.readFile('./Files/input.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading file:', err);
+        return;
+    }
+    console.log('File contents:', data);
+});
