460704f58d4981656c58da9bea920217e6571997
Shishir <shishirkumar123@gmail.com>
2024-09-13 15:41:11 -0700

    c

--- basic/app2a.js
+++ basic/app2a.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 // Read a file asynchronously
-fs.readFile('./Files/input.txt', 'utf8', (err, data) => {
+fs.readFile('./Files/input.txt', 'utf8', (err, data) => {//put the wrong file path and see how error is handled
     if (err) {
         console.error('Error reading file:', err);
         return;
